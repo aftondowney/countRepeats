b1fc3d7667c67a0e7123c4c97806ac1be1b49abb
Afton Downey <aaike001@gmail.com>
2016-02-19 10:37:09 -0800

    pass all tests by removing punctuation

--- src/Count.php
+++ src/Count.php
@@ -5,9 +5,9 @@
         function countWord($input_sentence, $input_find)
         {
             $sentence = strtolower($input_sentence);
+            $sentence = preg_replace("/[^a-zA-Z0-9\s]/", "", $sentence);
             $find = strtolower($input_find);
             $sentence = explode(" ", $sentence);
-            var_dump($sentence);
             $matched_words = array();
             foreach ($sentence as $word){
                 if ($word == $find){
