9407d44b939df56fd9359bdff2f70f32a0ec216d
Afton Downey <aaike001@gmail.com>
2016-02-19 09:17:01 -0800

    add initial setup to test and class

---
+++ tests/CountTest.php
@@ -0,0 +1,20 @@
+<?php
+    require_once "src/Count.php";
+
+    class CountTest extends PHPUnit_Framework_TestCase
+    {
+        function test_count_one_word()
+        {
+            // Arrange
+            $test_Count = new Count;
+            $sentence =
+            $find =
+
+            // Act
+            $result = $test_Count->countWord($sentence, $find);
+
+            // Assert
+            $this->assertEquals(, $result);
+        }
+      }
+?>
