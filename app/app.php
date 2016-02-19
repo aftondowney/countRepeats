42bf3c768986b1289096cf355201d251db00b0d1
Afton <aaike001@gmail.com>
2016-02-19 14:07:42 -0800

    move includes folder to views, add styling

--- app/app.php
+++ app/app.php
@@ -9,6 +9,7 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig');
     });
@@ -16,7 +17,7 @@
     $app->get("/find", function() use ($app){
         $new_sentence = new Count;
         $new_input = $new_sentence->countWord($_GET['sentence'], $_GET['find']);
-        return $app['twig']->render('index.html.twig', array('results' => $new_input));
+        return $app['twig']->render('index.html.twig', array('results' => $new_input, 'sentence' => $_GET['sentence']));
       });
 
     return $app;
